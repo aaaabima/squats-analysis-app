2e90319afdf1473ac3acf495e4b089e7a5a1de44
aaaabima <aaaabim@gmail.com>
2023-07-14 02:14:34 +0700

    feat: implement schedule on fragment

--- app/src/main/java/id/aaaabima/squats_analysis_app/fragment/schedule/ScheduleFragment.kt
+++ app/src/main/java/id/aaaabima/squats_analysis_app/fragment/schedule/ScheduleFragment.kt
@@ -6,13 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import id.aaaabima.squats_analysis_app.R
+import id.aaaabima.squats_analysis_app.databinding.FragmentScheduleBinding
 
 
-class ScheduleFragment : Fragment() {
+class ScheduleFragment : Fragment(), View.OnClickListener {
 
+    private var binding: FragmentScheduleBinding? = null
+    private lateinit var alarmReceiver: AlarmReceiver
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = FragmentScheduleBinding.inflate(layoutInflater)
+
+        binding?.btnScheduleDate?.setOnClickListener(this)
+        binding?.btnScheduleTime?.setOnClickListener(this)
+        binding?.btnSetSchedule?.setOnClickListener(this)
+        binding?.btnDailyTime?.setOnClickListener(this)
+        binding?.btnSetDaily?.setOnClickListener(this)
+
+        alarmReceiver = AlarmReceiver()
     }
 
     override fun onCreateView(
@@ -20,10 +32,61 @@ class ScheduleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false)
+        return binding?.root
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_schedule_date -> {
+                val datePickerFragment = DatePickerFragment()
+                datePickerFragment.show(childFragmentManager, DATE_PICKER_TAG)
+            }
+
+            R.id.btn_schedule_time -> {
+                val timePickerFragment = TimePickerFragment()
+                timePickerFragment.show(childFragmentManager, TIME_PICKER_ONCE_TAG)
+            }
+
+            R.id.btn_set_schedule -> {
+                val scheduleDate = binding?.tvScheduleDate?.text.toString()
+                val scheduleTime = binding?.tvScheduleTime?.text.toString()
+                val scheduleMessage = binding?.edtScheduleDetails?.text.toString()
+
+                alarmReceiver.setOneTimeAlarm(
+                    requireContext(),
+                    AlarmReceiver.TYPE_ONE_TIME,
+                    scheduleDate,
+                    scheduleTime,
+                    scheduleMessage
+                )
+            }
+
+            R.id.btn_daily_time -> {
+                val timePickerFragmentRepeat = TimePickerFragment()
+                timePickerFragmentRepeat.show(childFragmentManager, TIME_PICKER_REPEAT_TAG)
+            }
+
+            R.id.btn_set_daily -> {
+                val dailyTime = binding?.tvDailyTime?.text.toString()
+                val dailyMessage = binding?.edtDailyMessage?.text.toString()
+                alarmReceiver.setRepeatingAlarm(
+                    requireContext(),
+                    AlarmReceiver.TYPE_REPEATING,
+                    dailyTime,
+                    dailyMessage
+                )
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 
     companion object {
-
+        private const val DATE_PICKER_TAG = "DatePicker"
+        private const val TIME_PICKER_ONCE_TAG = "TimePickerOnce"
+        private const val TIME_PICKER_REPEAT_TAG = "TimePickerRepeat"
     }
 }
