6363946e3d124afcdc07150f942c0ee8ad91300d
aaaabima <aaaabim@gmail.com>
2023-07-13 22:36:31 +0700

    feat: add overlay view class

---
+++ app/src/main/java/id/aaaabima/squats_analysis_app/OverlayView.kt
@@ -0,0 +1,9 @@
+package id.aaaabima.squats_analysis_app
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class OverlayView(context: Context?, attrs: AttributeSet): View(context, attrs) {
+
+}
