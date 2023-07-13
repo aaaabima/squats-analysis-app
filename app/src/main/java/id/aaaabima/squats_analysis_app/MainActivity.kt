f9750b2d19ff13023498cb3d2962ce22de321859
aaaabima <aaaabim@gmail.com>
2023-07-14 00:59:17 +0700

    feat: set bottom navigation with nav component

--- app/src/main/java/id/aaaabima/squats_analysis_app/MainActivity.kt
+++ app/src/main/java/id/aaaabima/squats_analysis_app/MainActivity.kt
@@ -3,20 +3,32 @@ package id.aaaabima.squats_analysis_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import id.aaaabima.squats_analysis_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.permissionFragment,
+                R.id.fragment_exercise,
+                R.id.fragment_schedule,
+                R.id.fragment_settings
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
         binding.bottomNavMain.setupWithNavController(navController)
     }
 
