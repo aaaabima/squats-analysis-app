bb2804800f6c2e19da2e38dcfd52a7c90946cbcd
aaaabima <aaaabim@gmail.com>
2023-07-13 22:33:54 +0700

    feat: change main activity

--- app/src/main/java/id/aaaabima/squats_analysis_app/MainActivity.kt
+++ app/src/main/java/id/aaaabima/squats_analysis_app/MainActivity.kt
@@ -2,10 +2,26 @@ package id.aaaabima.squats_analysis_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import id.aaaabima.squats_analysis_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavMain.setupWithNavController(navController)
+    }
+
+    @Deprecated("Deprecated in Java", ReplaceWith("finish()"))
+    override fun onBackPressed() {
+        finish()
     }
 }
