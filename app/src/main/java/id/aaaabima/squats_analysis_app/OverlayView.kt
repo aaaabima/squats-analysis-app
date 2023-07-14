4a7786bfcb94674cd16f8bd71fd84faf8d1395c6
aaaabima <aaaabim@gmail.com>
2023-07-14 21:52:59 +0700

    feat: implement overlay view helper function

--- app/src/main/java/id/aaaabima/squats_analysis_app/OverlayView.kt
+++ app/src/main/java/id/aaaabima/squats_analysis_app/OverlayView.kt
@@ -1,9 +1,87 @@
 package id.aaaabima.squats_analysis_app
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.ContextCompat
+import com.google.mediapipe.tasks.vision.poselandmarker.PoseLandmarker
+import com.google.mediapipe.tasks.vision.poselandmarker.PoseLandmarkerResult
+import java.lang.Float.max
 
 class OverlayView(context: Context?, attrs: AttributeSet): View(context, attrs) {
+    private var results: PoseLandmarkerResult? = null
+    private var pointPaint = Paint()
+    private var linePaint = Paint()
 
+    private var scaleFactor: Float = 1f
+    private var imageWidth: Int = 1
+    private var imageHeight: Int = 1
+
+    init {
+        initPaints()
+    }
+
+    fun clear() {
+        results = null
+        pointPaint.reset()
+        linePaint.reset()
+        invalidate()
+        initPaints()
+    }
+
+    private fun initPaints() {
+        linePaint.color = ContextCompat.getColor(context!!, androidx.appcompat.R.color.material_blue_grey_800)
+        linePaint.strokeWidth = LANDMARK_STROKE_WIDTH
+        linePaint.style = Paint.Style.STROKE
+
+        pointPaint.color = Color.YELLOW
+        pointPaint.strokeWidth = LANDMARK_STROKE_WIDTH
+        pointPaint.style = Paint.Style.FILL
+    }
+
+    override fun draw(canvas: Canvas) {
+        super.draw(canvas)
+        results?.let { poseLandmarkerResult ->
+            for (landmark in poseLandmarkerResult.landmarks()) {
+                for (normalizedLandmark in landmark) {
+                    canvas.drawPoint(
+                        normalizedLandmark.x() * imageHeight * scaleFactor,
+                        normalizedLandmark.y() * imageHeight * scaleFactor,
+                        pointPaint
+                    )
+                }
+
+                PoseLandmarker.POSE_LANDMARKS.forEach {
+                    canvas.drawLine(
+                        poseLandmarkerResult.landmarks()[0][it!!.start()].x() * imageWidth * scaleFactor,
+                        poseLandmarkerResult.landmarks()[0][it.start()].y() * imageHeight * scaleFactor,
+                        poseLandmarkerResult.landmarks()[0][it.end()].x() * imageWidth * scaleFactor,
+                        poseLandmarkerResult.landmarks()[0][it.end()].y() * imageHeight * scaleFactor,
+                        linePaint
+                    )
+                }
+            }
+        }
+    }
+
+    fun setResults(
+        poseLandmarkerResult: PoseLandmarkerResult,
+        imageHeight: Int,
+        imageWidth: Int,
+    ) {
+        results = poseLandmarkerResult
+
+        this.imageHeight = imageHeight
+        this.imageWidth = imageWidth
+
+        scaleFactor = max(width * 1f / imageWidth, height * 1f / imageHeight)
+        invalidate()
+    }
+
+    companion object {
+        private const val LANDMARK_STROKE_WIDTH = 12F
+    }
 }
