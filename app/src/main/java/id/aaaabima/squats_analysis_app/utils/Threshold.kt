42cd9a4fe29d0ad17b1eafee215408fb0a4965c8
aaaabima <aaaabim@gmail.com>
2023-07-17 16:34:26 +0700

    feat: add Threshold data class

---
+++ app/src/main/java/id/aaaabima/squats_analysis_app/utils/Threshold.kt
@@ -0,0 +1,10 @@
+package id.aaaabima.squats_analysis_app.utils
+
+data class Threshold(
+    val normalAngle: ArrayList<Int> = arrayListOf(0, 32),
+    val transAngle: ArrayList<Int> = arrayListOf(35, 65),
+    val passAngle: ArrayList<Int> = arrayListOf(70, 95),
+    val hipThresh: ArrayList<Int> = arrayListOf(10, 50),
+    val ankleThresh: ArrayList<Int> = arrayListOf(45),
+    val kneeThresh: ArrayList<Int> = arrayListOf(50, 70, 95)
+)
