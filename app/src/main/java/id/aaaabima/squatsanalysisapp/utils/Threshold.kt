a08356ddc8da3211e58f9700ae49e79a941c6dc2
aaaabima <aaaabim@gmail.com>
2023-07-19 19:05:26 +0700

    feat: change threshold to enum class

--- app/src/main/java/id/aaaabima/squatsanalysisapp/utils/Threshold.kt
+++ app/src/main/java/id/aaaabima/squatsanalysisapp/utils/Threshold.kt
@@ -1,10 +1,11 @@
 package id.aaaabima.squatsanalysisapp.utils
 
-data class Threshold(
+enum class Threshold(
     val normalAngle: ArrayList<Int> = arrayListOf(0, 32),
     val transAngle: ArrayList<Int> = arrayListOf(35, 65),
     val passAngle: ArrayList<Int> = arrayListOf(70, 95),
     val hipThresh: ArrayList<Int> = arrayListOf(10, 50),
     val ankleThresh: ArrayList<Int> = arrayListOf(45),
-    val kneeThresh: ArrayList<Int> = arrayListOf(50, 70, 95)
+    val kneeThresh: ArrayList<Int> = arrayListOf(50, 70, 95),
+    val offsetThresh: ArrayList<Float> = arrayListOf(35.0f)
 )
