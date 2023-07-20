ad65ec9e0cc96dced8930b299d0a568f9d20cda6
aaaabima <aaaabim@gmail.com>
2023-07-20 16:53:52 +0700

    fix: refactor enum class implementation

--- app/src/main/java/id/aaaabima/squatsanalysisapp/utils/Threshold.kt
+++ app/src/main/java/id/aaaabima/squatsanalysisapp/utils/Threshold.kt
@@ -1,11 +1,12 @@
 package id.aaaabima.squatsanalysisapp.utils
 
-enum class Threshold(
-    val normalAngle: ArrayList<Int> = arrayListOf(0, 32),
-    val transAngle: ArrayList<Int> = arrayListOf(35, 65),
-    val passAngle: ArrayList<Int> = arrayListOf(70, 95),
-    val hipThresh: ArrayList<Int> = arrayListOf(10, 50),
-    val ankleThresh: ArrayList<Int> = arrayListOf(45),
-    val kneeThresh: ArrayList<Int> = arrayListOf(50, 70, 95),
-    val offsetThresh: ArrayList<Float> = arrayListOf(35.0f)
-)
+enum class Threshold(val value: ArrayList<Number>) {
+    NormalAngle(value = arrayListOf(0, 32)),
+    TransAngle(value = arrayListOf(35, 65)),
+    PassAngle(value = arrayListOf(70, 95)),
+    HipThresh(value = arrayListOf(10, 50)),
+    AnkleThresh(value = arrayListOf(45)),
+    KneeThresh (value = arrayListOf(50, 70, 95)),
+    OffsetThresh (value = arrayListOf(35.0f))
+}
+
