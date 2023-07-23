fca8617e3ca63122cb8fc3eead8381a3d9f3a800
aaaabima <aaaabim@gmail.com>
2023-07-23 20:54:15 +0700

    feat: remove unused function

--- app/src/main/java/id/aaaabima/squatsanalysisapp/PoseLandmarkerHelper.kt
+++ app/src/main/java/id/aaaabima/squatsanalysisapp/PoseLandmarkerHelper.kt
@@ -3,8 +3,6 @@ package id.aaaabima.squatsanalysisapp
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Matrix
-import android.media.MediaMetadataRetriever
-import android.net.Uri
 import android.os.SystemClock
 import android.util.Log
 import androidx.annotation.VisibleForTesting
@@ -107,7 +105,7 @@ class PoseLandmarkerHelper(
     }
 
     // Convert ImageProxy to MP Image and feed it to PoseLandmarkerHelper
-    fun detecLiveStream(
+    fun detectLiveStream(
         imageProxy: ImageProxy,
         isFrontCamera: Boolean
     ) {
@@ -160,122 +158,6 @@ class PoseLandmarkerHelper(
         // will be returned in returnLivestreamResult function
     }
 
-    // Accepts the URI for video file loaded from user's gallery and attempts to run
-    // pose landmarker inference on the video. This process will evaluate every frame in video
-    // and attach result to bundle that will be returned
-    fun detectVideoFile(
-        videoUri: Uri,
-        inferenceIntervalMs: Long
-    ): ResultBundle? {
-        if (runningMode != RunningMode.VIDEO) {
-            throw IllegalArgumentException(
-                "Attempt to call detectVideoFile without RunningMode.VIDEO"
-            )
-        }
-
-        // Inference time is difference between system time at start and end of process
-        val startTime = SystemClock.uptimeMillis()
-
-        var didErrorOccured = false
-
-        // Load frames from video and run pose landmarker
-        val retriever = MediaMetadataRetriever()
-        retriever.setDataSource(context, videoUri)
-        val videoLengthMs =
-            retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)?.toLong()
-
-        // Width/height from frame is needed instead of video directly because MediaRetriever
-        // returns frames that are smaller than the actual dimension of video file
-        val firstFrame = retriever.getFrameAtTime(0)
-        val width = firstFrame?.width
-        val height = firstFrame?.height
-
-        // If the video is invalid, return null detection result
-        if ((videoLengthMs == null) || (width == null) || (height == null)) return null
-
-        // Next, get one frame every frame interval ms then run detection on these frames
-        val resultList = mutableListOf<PoseLandmarkerResult>()
-        val numberofFrameToRead = videoLengthMs.div(inferenceIntervalMs)
-
-        for (i in 0 .. numberofFrameToRead) {
-            val timestampMs = i * inferenceIntervalMs // ms
-
-            retriever
-                .getFrameAtTime(
-                    timestampMs * 1000, // Convert from ms to microsecond
-                    MediaMetadataRetriever.OPTION_CLOSEST
-                )
-                ?.let { frame ->
-                    // Convert video frame to ARGB_8888 which is required by the MediaPipe
-                    val argb8888Frame =
-                        if (frame.config == Bitmap.Config.ARGB_8888) frame
-                        else frame.copy(Bitmap.Config.ARGB_8888, false)
-
-                    // Convert the input Bitmap object to an MPImage object to run inference
-                    val mpImage = BitmapImageBuilder(argb8888Frame).build()
-
-                    // Run pose landmarker using MediaPipe Pose Landmarker API
-                    poseLandmarker?.detectForVideo(mpImage, timestampMs)
-                        ?.let { detectionResult ->
-                            resultList.add(detectionResult)
-                        } ?: {
-                            didErrorOccured = true
-                        poseLandmarkerHelperListener?.onError(
-                            "ResultBundle could not be returned" +
-                                    " in detectVideoFile"
-                        )
-                    }
-                }
-                ?: run {
-                    didErrorOccured = true
-                    poseLandmarkerHelperListener?.onError(
-                        "Frame at specified time could not be" +
-                                " retrieved when detecting in video."
-                    )
-                }
-        }
-
-        retriever.release()
-
-        val inferenceTimePerFrameMs = (SystemClock.uptimeMillis() - startTime).div(numberofFrameToRead)
-
-        return if (didErrorOccured) {
-            null
-        } else {
-            ResultBundle(resultList, inferenceTimePerFrameMs, height, width)
-        }
-    }
-
-    // Accepted a Bitmap and runs pose landmarker inference on it to return
-    // results back to caller
-    fun detectImage(image: Bitmap): ResultBundle? {
-        if (runningMode != RunningMode.IMAGE) {
-            throw IllegalArgumentException(
-                "Attempting to call detectImage" +
-                        " while not using RunningMode.IMAGE"
-            )
-        }
-
-        val startTime = SystemClock.uptimeMillis()
-
-        val mpImage = BitmapImageBuilder(image).build()
-
-        poseLandmarker?.detect(mpImage)?.also { landmarkResult ->
-            val inferenceTimeMs = SystemClock.uptimeMillis() - startTime
-            return ResultBundle(
-                listOf(landmarkResult),
-                inferenceTimeMs,
-                image.height,
-                image.width
-            )
-        }
-
-        poseLandmarkerHelperListener?.onError(
-            "Pose Landmarker failed to detect"
-        )
-        return null
-    }
-
     private fun returnLivestreamResult(
         result: PoseLandmarkerResult,
         input: MPImage
@@ -308,7 +190,6 @@ class PoseLandmarkerHelper(
         const val DEFAULT_POSE_DETECTION_CONFIDENCE = 0.5F
         const val DEFAULT_POSE_TRACKING_CONFIDENCE = 0.5F
         const val DEFAULT_POSE_PRESENCE_CONFIDENCE = 0.5F
-        const val DEFAULT_NUM_POSES = 1
         const val OTHER_ERROR = 0
         const val GPU_ERROR = 1
         const val DEFAULT_CAMERA_FACING_FRONT = CameraSelector.LENS_FACING_FRONT
